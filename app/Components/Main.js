4b7615be682585b81fa0fda0387635c3364e16fb
Samantha Capps <samantha@Cheez.local>
2017-07-16 18:43:25 -0400

    deployed

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -46,7 +46,6 @@ var Main = React.createClass({
 
 	componentDidUpdate: function(prevProps, prevState) {
 		if (trigger === false) {
-			console.log(NYTkey);
 			trigger = true;
 			var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 			url += "?" + $.param({
